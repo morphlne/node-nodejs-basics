bef51206afb1ac08dff1c67a37390454823625bd
morphlne <lsmrphln@gmail.com>
2023-06-16 17:47:08 +0200

    Command line interface

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,31 @@
 const parseArgs = () => {
     // Write your code here 
+
+    //slice for skip path to node bin & path to script
+    const args = process.argv.slice(2)
+
+    const keyPrefix = "--"
+
+    const map = new Map()
+    let previousKey = ""
+    
+    args.forEach(current => {
+        if (current.startsWith(keyPrefix)) {
+            const key = current.slice(2)
+            map[key] = ""
+            previousKey = key
+        } else {
+            map[previousKey] = current
+        }
+    })
+
+    const formatedArgs = []
+
+    for (const key in map) {
+        formatedArgs.push(`${key} is ${map[key]}`)
+    }
+
+    console.log(formatedArgs.join(", "))
 };
 
 parseArgs();
