ab2044eb140ac49143d68f2b772e93e2d3d08c66
morphlne <lsmrphln@gmail.com>
2023-06-17 19:40:26 +0200

    Modules

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,14 +1,17 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import * as path from "path"
+import { release, version } from "os"
+import { createServer } from "http"
+import { fileURLToPath } from "url"
+import { createRequire } from "module"
+import * as c from "./files/c.js"
 
 const random = Math.random();
 
-let unknownObject;
+export let unknownObject;
 
+const require = createRequire(import.meta.url)
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = require('./files/a.json')
 } else {
     unknownObject = require('./files/b.json');
 }
@@ -17,24 +20,21 @@ console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
-const myServer = createServerHttp((_, res) => {
+export const myServer = createServer((_, res) => {
     res.end('Request accepted');
 });
 
 const PORT = 3000;
 
-console.log(unknownObject);
+console.log(unknownObject)
 
 myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     console.log('To terminate it, use Ctrl+C combination');
 });
-
-module.exports = {
-    unknownObject,
-    myServer,
-};
-
