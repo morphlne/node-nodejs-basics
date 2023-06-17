cba899960561cbdba28f5fe0ed379cb470dd8f33
morphlne <lsmrphln@gmail.com>
2023-06-17 14:45:31 +0200

    Zlib

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { absolutePath } from "../fs/utility.js"
+import { createReadStream, createWriteStream } from "fs"
+import { createUnzip } from "zlib"
+
 const decompress = async () => {
     // Write your code here 
+
+    const relativeSource = "/src/zip/files/archive.gz"
+    const sourcePath = absolutePath(relativeSource)
+    const source = createReadStream(sourcePath)
+   
+    const relativeTarget = "/src/zip/files/fileToCompress_decompressed.txt"
+    const targetPath = absolutePath(relativeTarget)
+    const target = createWriteStream(targetPath)
+
+    const unzip = createUnzip()
+
+    source
+        .pipe(unzip)
+        .pipe(target)
 };
 
 await decompress();
