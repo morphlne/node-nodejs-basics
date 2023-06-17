25fef8c8813a23fe718e2bbeef42f5e073be0943
morphlne <lsmrphln@gmail.com>
2023-06-17 19:14:15 +0200

    Child Processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,10 @@
+import { fork } from "child_process"
+
 const spawnChildProcess = async (args) => {
     // Write your code here
+
+    const childPath = "./src/cp/files/script.js"
+    fork(childPath, args)
 };
 
 // Put your arguments in function call to test this functionality
