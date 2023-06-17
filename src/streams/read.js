a737aafcf457f4ce000c41cb5eebaea9444c845f
morphlne <lsmrphln@gmail.com>
2023-06-17 12:48:32 +0200

    Streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { absolutePath } from "../fs/utility.js"
+import { createReadStream } from "fs"
+import { stdout } from "process"
+
 const read = async () => {
     // Write your code here 
+
+    const relativePath = "/src/streams/files/fileToRead.txt"
+    const path = absolutePath(relativePath)
+    const encoding = "utf-8"
+
+    const stream = createReadStream(path, encoding)
+
+    stream.pipe(stdout)
 };
 
 await read();
